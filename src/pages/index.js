f7cf5ec157e8d3f93a8dbba0b80ccaedd9504536
Christian Aichner <contact@aichner-christian.com>
2020-09-23 18:38:33 +0200

    Improve base file
    Regions and copyright has been added.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
-import React from "react"
+//#region > Imports
+//> React
+// Contains all the functionality necessary to define React components
+import React from "react";
+//#endregion
 
+//#region > Exports
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>Hello world!</div>;
 }
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2020 InspireMedia GmbH
+ */
